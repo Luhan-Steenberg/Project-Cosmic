815597f214a49611a3e6813a7ca8a4e3fcdfb51d
Luhan Steenberg <luhansteenberg@icloud.com>
2026-04-12 07:45:15 +0200

    Coordinates base implementation

---
+++ coordinate.py
@@ -0,0 +1,9 @@
+class Coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({x} , {y})"
+
+Coord = Coordinate # type alias for easy reference
